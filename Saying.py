beadcaf9247e24cc41e83eab7f6d905613963ac5
mzikeks <mikhailcommercial@gmail.com>
2021-11-08 20:10:54 +0300

    fix logic

--- Saying.py
+++ Saying.py
@@ -1,14 +1,14 @@
-import StorehouseOfWisdom
+from StorehouseOfWisdom import StorehouseOfWisdom
 
 
 class Saying(StorehouseOfWisdom):
     def __init__(self, content, country):
+        super().__init__(content)
         self.country = country
-        super(content)
 
     def __str__(self):
         return f'It is Saying ' \
                f'from {self.country}: ' \
                f'{self.content}. ' \
-               f'Quotient = {self.getQuonitent()}'
+               f'Quotient = {self.getQuotient()}'
 
